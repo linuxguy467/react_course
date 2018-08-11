eaaf62b22717168d73011eebad66ef490af68628
linuxguy467 <dpplrdr435@gmail.com>
2018-08-10 22:34:09 -0400

    Added Login Form and reusable components for Login

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,19 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+  doSubmit = () => {
+    // Call server
+    console.log("Submitted");
+  };
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
-          <div className="form-group">
-            <label htmlFor="">Username</label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="">Password</label>
-            <input type="text" className="form-control" />
-          </div>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
